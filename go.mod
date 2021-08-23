c67f812c260543a71f788089f0b681dc0ff7735b
Loki <stalker.loki@protonmail.ch>
2021-08-23 19:12:47 +0300

    fully working request rewrites for git top level path

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/cybriq/rproxy
 go 1.16
 
 require (
-	github.com/davecgh/go-spew v1.1.0
+	github.com/bogdanovich/dns_resolver v0.0.0-20170211073258-a8e42bc6a5b6
+	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/labstack/echo/v4 v4.5.0
+	github.com/miekg/dns v1.1.43 // indirect
 )
--- main.go
+++ main.go
@@ -1,102 +1,79 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
 	"net/url"
 	"strings"
 )
 
-type (
-	proxySpec struct {
-		path   string
-		target *url.URL
+// NewProxy takes target host and creates a reverse proxy
+func NewProxy(targetHost string) (*httputil.ReverseProxy, error) {
+	url, err := url.Parse(targetHost)
+	if err != nil {
+		return nil, err
 	}
-)
-
-const (
-	listenHost = "cybriq.systems"
-	listenPort = "443"
-)
-
-type (
-	Host struct {
-		Echo *echo.Echo
-	}
-)
-
-var (
-	proxies = []proxySpec{
-		{"/", mustParseURL("http://localhost:8001")},
-		{"/git", mustParseURL("http://localhost:8002")},
-		{"/blog", mustParseURL("http://localhost:8003")},
-		{"/api", mustParseURL("http://localhost:8004")},
-	}
-)
-
-func main() {
-	// set up automatic redirect to https
-	es := echo.New()
-	es.Pre(middleware.HTTPSRedirect())
-	go es.Start(listenHost + ":80")
-	defer es.Close()
-	// set up https enabled reverse proxy
-	e := echo.New()
-
-	e.Use(middleware.Recover())
-	e.Use(middleware.Logger())
-	// set up reverse proxies
-	hosts := map[string]*Host{}
-	for i := range proxies {
-		ec := echo.New()
-		ec.Use(middleware.Logger())
-		ec.Use(middleware.Recover())
-		ec.Use(middleware.Proxy(
-			middleware.NewRandomBalancer(
-				[]*middleware.ProxyTarget{
-					{
-						Name: proxies[i].path,
-						URL:  proxies[i].target,
-						Meta: nil,
-					},
-				},
-			),
-		),
-		)
-		hosts[proxies[i].path] = &Host{ec}
-	}
-	e.Any("/*", func(c echo.Context) (err error) {
-		req := c.Request()
-		res := c.Response()
-		h := req.URL.Path
-		// fmt.Println(h)
-		// if the hostname is the non-path this reverse proxy will apply
-		host := hosts["/"]
-		for i := range proxies {
-			pth := proxies[i].path
-			if pth == "/" {
-				continue
-			}
-			if strings.HasPrefix(h, pth) {
-				// fmt.Println(pth)
-				host = hosts[pth]
-				break
-			}
-		}
-		host.Echo.ServeHTTP(res, req)
-		return
-	},
-	)
-	e.Logger.Fatal(e.StartTLS(listenHost+":"+listenPort, "/home/loki/cybriq_systems/deploy/cybriq_systems.crt",
-		"/home/loki/cybriq_systems/deploy/cybriq_systems.key",
-	),
-	)
+	
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	proxy.ModifyResponse = modifyResponse()
+	proxy.Director = director()
+	return proxy, nil
 }
 
-func mustParseURL(u string) *url.URL {
-	url1, err := url.Parse(u)
+func director() func(*http.Request) {
+	return func(req *http.Request) {
+		// spew.Dump(req)
+		req.URL.Scheme = "http"
+		// req.URL.Host = "localhost:3000"
+		// fmt.Println(req.URL.String())
+		if strings.HasPrefix(req.URL.Path, "/git") {
+			req.URL.Host = "localhost:3000"
+			fmt.Println(req.URL.Path)
+			split := strings.Split(req.URL.Path, "/git")
+			if len(split) == 2 {
+				req.URL.Path = strings.Join(split[1:], "")+"/"
+			}
+			fmt.Println(req.URL)
+			// req.URL.Path
+		}
+		
+		// if strings.HasPrefix(req.URL.Path, "/") {
+		// 	req.URL.Host = target2.Host
+		// }
+	}
+}
+
+func modifyResponse() func(*http.Response) error {
+	return func(resp *http.Response) error {
+
+		// resp.Header.Set("X-Proxy", "Magical")
+		return nil
+	}
+}
+
+// ProxyRequestHandler handles the http request using proxy
+func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	}
+}
+
+func main() {
+	// initialize a reverse proxy and pass the actual backend server url here
+	proxy, err := NewProxy("http://localhost:3000")
 	if err != nil {
 		panic(err)
 	}
-	return url1
+	
+	// handle all requests to your server using the proxy
+	http.HandleFunc("/", ProxyRequestHandler(proxy))
+	log.Fatal(http.ListenAndServeTLS(
+		":443",
+		"/home/loki/cybriq_systems/deploy/cybriq_systems.crt",
+		"/home/loki/cybriq_systems/deploy/cybriq_systems.key",
+		nil,
+	),
+	)
 }
